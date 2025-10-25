7d3d0392bea02e3577205f0a3c674219674810b6
Rodrigo Griesi <56765588+xrodz@users.noreply.github.com>
2025-10-25 12:30:33 -0400

    Remove explanatory comments from medium modules
    Deleted the block of comments from the docstrings in medium_level0.py, medium_level1.py, and two medium_bccgranule.py files to streamline documentation and reduce redundancy.

--- openwave/spacetime/medium_level1.py
+++ openwave/spacetime/medium_level1.py
@@ -7,8 +7,4 @@ Objects Engine @spacetime module.
 
 WAVE-MEDIUM propagates Wave Motion (ENERGY-WAVE).
 Modeled as a fluid-like medium that allows energy to transfer from one point to the next.
-
-"Aether" can refer to the personification of the bright upper sky in Greek mythology,
-the classical fifth element or quintessence filling the universe,
-or a hypothetical substance once thought to carry light and other electromagnetic waves.
 """
