296d2c2f7f55cc949fb04031a2fbd6145ee2a7c6
Rodrigo Griesi <56765588+xrodz@users.noreply.github.com>
2025-11-24 18:40:06 -0500

    Set simulation to paused by default
    Changed the default value of the 'PAUSED' parameter from False to True in the energy_wave.py configuration. This ensures the simulation starts in a paused state.

--- openwave/xperiments/5_level1_wave_field/_xparameters/energy_wave.py
+++ openwave/xperiments/5_level1_wave_field/_xparameters/energy_wave.py
@@ -29,7 +29,7 @@ XPARAMETERS = {
         "FLUX_MESH_OPTION": 1,  # Flux Mesh toggle, 0: none, 1: xy, 2: xy+xz, 3: xy+xz+yz
         "FREQ_BOOST": 1.0,  # Frequency boost multiplier
         "AMP_BOOST": 1.0,  # Amplitude boost multiplier
-        "PAUSED": False,  # Pause/Start simulation toggle
+        "PAUSED": True,  # Pause/Start simulation toggle
     },
     "color_defaults": {
         "COLOR_THEME": "OCEAN",  # Choose color theme for rendering (OCEAN, DESERT, FOREST)
