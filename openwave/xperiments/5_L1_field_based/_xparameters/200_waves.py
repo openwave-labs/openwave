d100df507169f66c63adaf4062ea003665cc5925
Rodrigo Griesi <56765588+xrodz@users.noreply.github.com>
2025-12-15 16:55:50 -0500

    Add transverse wave fields and amplitude tracking
    Introduced psiT fields for transverse wave support in WaveField and enabled amplitude and RMS tracking for transverse waves in Trackers. Also added experiment parameters for a 200-wave simulation in a new configuration file.

---
+++ openwave/xperiments/5_L1_field_based/_xparameters/200_waves.py
@@ -0,0 +1,44 @@
+"""
+XPERIMENT PARAMETERS
+
+This XPERIMENT showcases:
+-
+"""
+
+UNIVERSE_EDGE = 5.7e-15  # m, universe edge length in meters
+
+XPARAMETERS = {
+    "meta": {
+        "X_NAME": "200 waves, 200M voxels",
+        "DESCRIPTION": "Energy Wave Charging, Propagation and Interaction",
+    },
+    "camera": {
+        "INITIAL_POSITION": [1.40, 1.40, 1.20],  # [x, y, z] in normalized coordinates
+    },
+    "universe": {
+        "SIZE": [UNIVERSE_EDGE, UNIVERSE_EDGE, UNIVERSE_EDGE],  # m, simulation domain [x, y, z]
+        "TARGET_VOXELS": 2e8,  # Simulation voxel count (impacts performance)
+    },
+    "charging": {
+        "STATIC_BOOST": 1.00,  # One-Time charger amplitude boost multiplier
+    },
+    "ui_defaults": {
+        "SHOW_AXIS": False,  # Toggle to show/hide axis lines
+        "TICK_SPACING": 0.25,  # Axis tick marks spacing for position reference
+        "SHOW_GRID": False,  # Toggle to show/hide the voxel data-grid
+        "SHOW_EDGES": False,  # Toggle to show/hide universe edges
+        "SHOW_FLUX_MESH": 1,  # Flux Mesh toggle, 0: none, 1: xy, 2: xy+xz, 3: xy+xz+yz
+        "FLUX_MESH_PLANES": [0.5, 0.5, 0.5],  # [x, y, z] positions relative to universe size
+        "SIM_SPEED": 1.0,  # Simulation speed multiplier
+        "PAUSED": False,  # Pause/Start simulation toggle
+    },
+    "color_defaults": {
+        "COLOR_THEME": "OCEAN",  # Choose color theme for rendering (OCEAN, DESERT, FOREST)
+        "COLOR_PALETTE": 1,  # Color palette list: redshift (1), ironbow (2), blueprint (3), viridis (4), orange (5)
+    },
+    "analytics": {
+        "INSTRUMENTATION": False,  # Toggle data acquisition and analytics
+        "EXPORT_VIDEO": False,  # Toggle frame image export to video directory
+        "VIDEO_FRAMES": 24,  # Target frame number to stop recording and finalize video export
+    },
+}
